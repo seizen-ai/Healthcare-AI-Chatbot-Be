190056039ea1678f6bb030ec4ab44b86b16ec591
vaishnav <khannavaishnav7@gmail.com>
2026-09-22 20:35:59 +0530

    fix : updated gmail smtp to sendgrid smtp for production

--- modules/notification/config/email.config.js
+++ modules/notification/config/email.config.js
@@ -8,13 +8,20 @@ const getFromAddress = () => {
 
 const emailConfig = {
     from: {
-        name: process.env.SMTP_FROM_NAME || "Healthcare AI Chatbot",
+        name: isProduction ? process.env.SENDGRID_FROM : (process.env.SMTP_FROM_NAME || "Seizen-AI"),
         address: getFromAddress()
     },
 
-    transport: {
-        service: process.env.SMTP_SERVICE || "gmail",
+    transport: isProduction ? {
+        host: "smtp.sendgrid.net",
         port: 2525,
+        auth: {
+            user: "apikey",
+            pass: process.env.SENDGRID_API_KEY
+        }
+    } : {
+        host: "smtp.gmail.com",
+        port: 587,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
--- modules/notification/email/email.service.js
+++ modules/notification/email/email.service.js
@@ -1,14 +1,14 @@
 import emailTransporter from "./email.transporter.js";
 import emailConfig from "../config/email.config.js";
 import { getEmailTemplate } from "./email.templates.js";
-
+const isProduction = process.env.NODE_ENV === "production";
 export class EmailService {
 
     async send({ to, subject, text, html }) {
         const transporter = emailTransporter.getTransporter();
 
         await transporter.sendMail({
-            from: `"${emailConfig.from.name}" <${emailConfig.from.address}>`,
+            from: `${isProduction ? `${emailConfig.from.name}` : `${emailConfig.from.name} <${emailConfig.from.address}>`}`,
             to,
             subject,
             text,
